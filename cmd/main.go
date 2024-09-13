39a53084cf4ef1b4debb64c8100e01035393e60e
Marcin Rataj <lidel@lidel.org>
2024-09-13 23:29:38 +0200

    fix: apply p2pforge before file plugin
    this allows us to handle acme and a records before looking
    into a static zone file

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,31 @@ import (
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/coremain"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 )
 
-var customDirectives = []string{
+var p2pForgeDirectives = []string{
 	"ipparser",
 	"acme",
 }
 
 func init() {
-	dnsserver.Directives = append(dnsserver.Directives, customDirectives...)
+	// Add custom plugins before 'file' to ensure our dynamic
+	// code is executed before static records loaded via 'file' which does not
+	// support fallthrough
+	// https://github.com/coredns/coredns/blob/v1.11.3/plugin.cfg
+	// https://github.com/coredns/coredns/issues/3601
+	for i, d := range dnsserver.Directives {
+		if d == "file" {
+			ds := make([]string, 0, len(dnsserver.Directives)+len(p2pForgeDirectives))
+			ds = append(ds, dnsserver.Directives[:i]...)
+			ds = append(ds, p2pForgeDirectives...)
+			ds = append(ds, dnsserver.Directives[i:]...)
+			dnsserver.Directives = ds
+			break
+		}
+	}
+	clog.Debugf("updated directives: %v", dnsserver.Directives)
 }
 
 func main() {
